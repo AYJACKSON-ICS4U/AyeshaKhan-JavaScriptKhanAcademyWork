e164b8679175bd4bab495591df76b8c1bcc77103
Ayesha Khan <31661317+AyeshaKhan7@users.noreply.github.com>
2017-09-11 11:24:21 -0400

    Create ExplodingSun.js

---
+++ ExplodingSun.js
@@ -0,0 +1,22 @@
+//Ayesha Khan
+//11/9/17
+
+noStroke();
+// the beautiful blue sky
+background(82, 222, 240);
+
+// the starting size for the sun
+var sunSize = 30; 
+
+//functiong draw
+draw = function() {
+    //loop
+    // The sun, a little circle on the horizon
+    fill(255, 204, 0);
+    ellipse(200, 298, sunSize, sunSize);
+    
+    // The land, blocking half of the sun
+    fill(76, 168, 67);
+    rect(0, 300, 400, 100);
+    sunSize = sunSize + 1;
+};
