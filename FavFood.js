74a763e618d7c5036eb15c23f06bde113c9ef885
Ayesha Khan <31661317+AyeshaKhan7@users.noreply.github.com>
2017-09-18 09:52:22 -0400

    Create FavFood.js

---
+++ FavFood.js
@@ -0,0 +1,13 @@
+// 9/18/17
+//Ayesha Khan
+
+fill(33, 32, 32);
+textSize(20);
+text("My Favourite Foods", 14, 24); //header
+
+textSize(15);
+text("1. Haleem",14, 55);//food 1
+//food2
+text("2. oranges", 14, 73);
+//food3
+text("3. peaches", 14, 93);
